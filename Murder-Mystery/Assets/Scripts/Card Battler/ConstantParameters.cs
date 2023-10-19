b39eb3712efaf80e78d5a9355719940b1145af1d
amonjerro <amonjerro@hotmail.com>
2023-10-19 18:47:48 -0400

    added turns parameter

--- Murder-Mystery/Assets/Scripts/Card Battler/ConstantParameters.cs
+++ Murder-Mystery/Assets/Scripts/Card Battler/ConstantParameters.cs
@@ -3,4 +3,5 @@ public class ConstantParameters
     public const int MAX_HAND_SIZE = 7;
     public const int MIN_FACE_VALUE = 3;
     public const int MAX_FACE_VALUE = 10;
+    public const int MAX_TURNS = 5;
 }
